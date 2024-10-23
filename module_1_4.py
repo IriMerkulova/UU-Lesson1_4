e7711dacd76b3147b942cb2522a8fcf017c9b3ac
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-10-23 16:03:04 +0300

    Add files via upload
    Переменные.

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+completedHomework = 12
+hoursSpent = 1.5
+CourseName = 'Python'
+TimeForOneTask = hoursSpent / completedHomework
+print('Курс:',CourseName, ', всего задач:',completedHomework,', затрачено часов:',hoursSpent,', среднее время выполнения',TimeForOneTask,'часа.')
