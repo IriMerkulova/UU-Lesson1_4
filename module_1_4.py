855e8b723bc0e2337a9423a12a93485d0d330328
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-10-24 17:45:49 +0300

    Update module_1_4.py
    исправление название переменных

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,5 @@
-completedHomework = 12
-hoursSpent = 1.5
-CourseName = 'Python'
-TimeForOneTask = hoursSpent / completedHomework
-print('Курс:',CourseName, ', всего задач:',completedHomework,', затрачено часов:',hoursSpent,', среднее время выполнения',TimeForOneTask,'часа.')
+completed_homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent / completed_homework
+print('Курс:', course_name, ', всего задач:', completed_homework, ', затрачено часов:', hours_spent, ', среднее время выполнения', time_for_one_task, 'часа.')
